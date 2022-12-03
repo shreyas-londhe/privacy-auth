b72b1a28998bf1b49ef7d13a7a8f12ba8f8b2e1a
Shreyas Londhe <62744899+shryasss@users.noreply.github.com>
2022-12-03 19:19:44 +0530

    Feat: deploy script completed and contract deployed

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -1,11 +1,49 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
-    solidity: "0.8.9",
+    solidity: {
+        version: "0.8.9",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+        },
+    },
+    networks: {
+        hardhat: {
+            forking: {
+                url: process.env.MAINNET_URL || "",
+                blockNumber: 15102500,
+            },
+            // blockGasLimit: 30000000
+        },
+        goerli: {
+            url: process.env.GOERLI_URL || "",
+            accounts:
+                process.env.PRIVATE_KEY !== undefined
+                    ? [process.env.PRIVATE_KEY]
+                    : [],
+        },
+        mumbai: {
+            url: process.env.MUMBAI_URL || "",
+            accounts:
+                process.env.PRIVATE_KEY !== undefined
+                    ? [process.env.PRIVATE_KEY]
+                    : [],
+        },
+    },
     gasReporter: {
-        enabled: true,
+        enabled: process.env.REPORT_GAS !== undefined,
         currency: "USD",
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
+    },
+    mocha: {
+        timeout: 10000000000,
     },
 };
 
